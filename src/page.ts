7f15f20453c6e28595b33b9d7758ac90b8566d98
Kot C <kot@yukata.dev>
2023-08-20 13:49:07 -0700

    Update padding demo url

--- src/page.ts
+++ src/page.ts
@@ -95,12 +95,14 @@ export default (c: Context<{ Bindings: Env }>) => {
             Just use the query parameters <code>pad</code>, like this:
             <code>https://${c.env.DOMAIN}/get/@:name?pad=9</code>
             Or <code>?pad=0</code> for no padding.
+            <h5>9 digits (pad=9)</h5>
             <img
-              src="//${c.env.DOMAIN}/get/@demo?pad=9"
+              src="//${c.env.DOMAIN}/get/@demo.pad?pad=9"
               alt="padding 9"
             />
+            <h5>No padding (pad=0)</h5>
             <img
-              src="//${c.env.DOMAIN}/get/@demo?pad=0"
+              src="//${c.env.DOMAIN}/get/@demo.pad?pad=0"
               alt="padding 0"
             />
           </p>
